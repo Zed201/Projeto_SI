6f0a1a99f08323b42b62b9aef9b77c369cb6870c
Luiz Gustavo Pinheiro <luiz.gustavopinheiro@ufpe.br>
2026-04-27 16:28:53 -0300

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Projeto_SI
+Codigo inicial para o projeto, podemos rodar ele usando `python -m http.server`
---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 200);
+  background(220);
+}
+
+function draw() {
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text("Hello World!", width / 2, height / 2);
+}
